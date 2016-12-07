e0b97ae157bcda95bec642846c143f97a5dbd10c
Dylan Graham <dylan.graham@gmail.com>
2016-12-07 13:11:46 +1100

    Lambdas

--- app/src/main/java/com/github/oliveiradev/rxphoto/MainActivity.java
+++ app/src/main/java/com/github/oliveiradev/rxphoto/MainActivity.java
@@ -1,21 +1,15 @@
 package com.github.oliveiradev.rxphoto;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Pair;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.github.oliveiradev.lib.RxPhoto;
-import com.github.oliveiradev.lib.compressor.SimpleCompressor;
 import com.github.oliveiradev.lib.shared.TypeRequest;
 
-import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,75 +22,43 @@ public class MainActivity extends AppCompatActivity {
         final LinearLayout thumbsContent = (LinearLayout) findViewById(R.id.thumbs);
         final ImageView image = (ImageView) findViewById(R.id.image);
 
-        findViewById(R.id.get).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                thumbsContent.removeAllViews();
-                RxPhoto.requestBitmap(v.getContext(), TypeRequest.GALLERY)
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .doOnNext(new Action1<Bitmap>() {
-                            @Override
-                            public void call(Bitmap bitmap) {
-                                image.setImageBitmap(bitmap);
-                            }
-                        })
-                        .subscribe();
-            }
+        findViewById(R.id.get).setOnClickListener(v -> {
+            thumbsContent.removeAllViews();
+            RxPhoto.requestBitmap(v.getContext(), TypeRequest.GALLERY)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnNext(image::setImageBitmap)
+                    .subscribe();
         });
 
-        findViewById(R.id.take).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                thumbsContent.removeAllViews();
-                RxPhoto.requestBitmap(v.getContext(), TypeRequest.CAMERA)
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .doOnNext(new Action1<Bitmap>() {
-                            @Override
-                            public void call(Bitmap bitmap) {
-                                image.setImageBitmap(bitmap);
-                            }
-                        })
-                        .subscribe();
-            }
+        findViewById(R.id.take).setOnClickListener(v -> {
+            thumbsContent.removeAllViews();
+            RxPhoto.requestBitmap(v.getContext(), TypeRequest.CAMERA)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnNext(image::setImageBitmap)
+                    .subscribe();
         });
 
-        findViewById(R.id.get_thumb).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RxPhoto.requestThumbnails(v.getContext(), TypeRequest.GALLERY,
-                        new Pair(60, 60), new Pair(120, 120), new Pair(240, 240))
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .doOnNext(new Action1<Bitmap>() {
-                            @Override
-                            public void call(Bitmap bitmap) {
-                                final ImageView newImage = new ImageView(MainActivity.this);
-                                newImage.setImageBitmap(bitmap);
-                                newImage.setPadding(10,10,10,10);
-                                thumbsContent.addView(newImage);
-                            }
-                        })
-                        .subscribe();
-            }
-        });
+        findViewById(R.id.get_thumb).setOnClickListener(v -> RxPhoto.requestThumbnails(v.getContext(), TypeRequest.GALLERY,
+                new Pair(60, 60), new Pair(120, 120), new Pair(240, 240))
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(bitmap -> {
+                    final ImageView newImage = new ImageView(MainActivity.this);
+                    newImage.setImageBitmap(bitmap);
+                    newImage.setPadding(10,10,10,10);
+                    thumbsContent.addView(newImage);
+                })
+                .subscribe());
 
-        findViewById(R.id.transform).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RxPhoto.requestBitmap(v.getContext(), TypeRequest.GALLERY)
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .flatMap(RxPhoto.transformToThumbnail(new Pair(240, 240), new Pair(120, 120), new Pair(60, 60)))
-                        .doOnNext(new Action1<Bitmap>() {
-                            @Override
-                            public void call(Bitmap bitmap) {
-                                final ImageView newImage = new ImageView(MainActivity.this);
-                                newImage.setImageBitmap(bitmap);
-                                newImage.setPadding(10,10,10,10);
-                                thumbsContent.addView(newImage);
-                            }
-                        })
-                        .subscribe();
-            }
-        });
+        findViewById(R.id.transform).setOnClickListener(v -> RxPhoto.requestBitmap(v.getContext(), TypeRequest.GALLERY)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(RxPhoto.transformToThumbnail(new Pair(240, 240), new Pair(120, 120), new Pair(60, 60)))
+                .doOnNext(bitmap -> {
+                    final ImageView newImage = new ImageView(MainActivity.this);
+                    newImage.setImageBitmap(bitmap);
+                    newImage.setPadding(10,10,10,10);
+                    thumbsContent.addView(newImage);
+                })
+                .subscribe());
     }
 }
